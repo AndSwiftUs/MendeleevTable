a316275a3531fb7ba0ec6f78690b84efc1de7997
Andrew <AndSwiftUs@gmail.com>
2022-10-25 12:13:09 +0300

    Initial Commit

---
+++ MendeleevTable Watch App/MendeleevTableApp.swift
@@ -0,0 +1,17 @@
+//
+//  MendeleevTableApp.swift
+//  MendeleevTable Watch App
+//
+//  Created by AndBelarUs on 25.10.2022.
+//
+
+import SwiftUI
+
+@main
+struct MendeleevTable_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ MendeleevTable/MendeleevTableApp.swift
@@ -0,0 +1,17 @@
+//
+//  MendeleevTableApp.swift
+//  MendeleevTable
+//
+//  Created by AndBelarUs on 25.10.2022.
+//
+
+import SwiftUI
+
+@main
+struct MendeleevTableApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
